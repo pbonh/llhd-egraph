b9a823d1d1436ad6496f3ffe3ffbe136c3679307
Phillip Bonhomme <phillip.bonhomme@gmail.com>
2025-01-17 00:56:13 +0000

    [Module,Scope] Re-export module type. Add from builder for LLHDScope.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 /// LLHD Utilities
 #[allow(dead_code)]
 pub mod llhd;
+pub use llhd::*;
 
 /// LLHD Program Egglog Representation
 #[allow(dead_code)]
--- src/llhd.rs
+++ src/llhd.rs
@@ -14,6 +14,7 @@ pub mod inst;
 
 /// LLHD Module Type Wrapper
 pub mod module;
+pub use module::*;
 
 /// LLHD Unit Data
 pub mod unit;
@@ -37,7 +38,9 @@ pub type LLHDValueRef = (UnitId, Inst, Value);
 pub type LLHDIndex = (UnitId, Value, Option<Inst>, Option<Value>);
 
 /// LLHD Scope
-#[derive(Debug, Clone, Default, Serialize, Deserialize)]
+#[derive(
+    Debug, Clone, Copy, Default, Hash, PartialEq, Eq, PartialOrd, Ord, Serialize, Deserialize,
+)]
 pub enum LLHDScope {
     #[default]
     Module,
@@ -45,4 +48,34 @@ pub enum LLHDScope {
     ValueDef(UnitId, Value),
     Inst(UnitId, Value, Inst),
     ValueRef(UnitId, Value, Inst, Value),
+}
+
+impl From<()> for LLHDScope {
+    fn from(_no_scope: ()) -> Self {
+        Self::Module
+    }
+}
+
+impl From<UnitId> for LLHDScope {
+    fn from(unit_id: UnitId) -> Self {
+        Self::Unit(unit_id)
+    }
+}
+
+impl From<(UnitId, Value)> for LLHDScope {
+    fn from(value_def: (UnitId, Value)) -> Self {
+        Self::ValueDef(value_def.0, value_def.1)
+    }
+}
+
+impl From<(UnitId, Value, Inst)> for LLHDScope {
+    fn from(inst_def: (UnitId, Value, Inst)) -> Self {
+        Self::Inst(inst_def.0, inst_def.1, inst_def.2)
+    }
+}
+
+impl From<(UnitId, Value, Inst, Value)> for LLHDScope {
+    fn from(value_ref: (UnitId, Value, Inst, Value)) -> Self {
+        Self::ValueRef(value_ref.0, value_ref.1, value_ref.2, value_ref.3)
+    }
 }
